2cf93b611c307015c8826a1e9e532218f3f82bd6
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-07 00:38:50 -0800

    Updated hero section

--- frontend/src/home/hero.js
+++ frontend/src/home/hero.js
@@ -2,8 +2,25 @@
 
 function HeroBanner() {
   return (
-    <div className="text-yellow-400 dark:text-green-500">
-      Hello, I'm Miki
+    <div className="">
+
+      {/* headline */}
+      <div className="flex flex-col lg:flex-row items-start p-6 text-gray-600 dark:text-gray-300">
+        <div className="text-left text-4xl font-bold">
+          <div>
+            Hello,
+          </div>
+          <div>
+            I'm Miki
+          </div>
+        </div>
+        <div className="w-4/5 mt-4 text-left text-sm">
+          I love to expand my knowledge of programming and enjoy exploring through building new projects.
+        </div>
+      </div>
+
+      {/* img */}
+      <div className="w-full h-96 bg-gray-200"></div>
     </div>
   );
 }
