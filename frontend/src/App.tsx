61a23c5f5e44dbef83555917519b21222f6384a1
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-07 00:03:52 -0800

    Updated darkMode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,30 @@
 import './App.css';
 import Home from './home/index.js';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DarkModeToggle from './utils/darkMode';
 
 function App() {
   const [darkMode, setDarkMode] = useState<boolean>(false);
 
+  useEffect(() => {
+    setDarkMode(localStorage.theme === 'dark' ?  true : false)
+  }, [localStorage.theme])
+
+  useEffect(() => {
+    const currentMode = localStorage.theme;
+    if (currentMode === 'dark') {
+      document.documentElement.style.background = '#1F2937';
+      document.documentElement.style.transition = 'background 200ms ease-in-out';
+    } else {
+      document.documentElement.style.background = '#fffef1';
+      document.documentElement.style.transition = 'background 200ms ease-in-out';
+    }
+  }, [darkMode]);
+
+
   return (
     <div className={`${darkMode && 'dark'} App`}>
-      <div className="flex justify-between items-center h-16 lg:h-40 p-4 bg-warmth">
+      <div className="flex justify-between items-center h-16 p-4">
         <div>Miki (logo)</div>
         <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode}  />
       </div>
--- frontend/src/utils/darkMode.tsx
+++ frontend/src/utils/darkMode.tsx
@@ -1,6 +1,6 @@
 import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
 
-function initDarkMode(): string {
+function initDarkMode(): void {
   // On page load or when changing themes, best to add inline in `head` to avoid FOUC
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     // document.documentElement.classList.add('dark')
@@ -9,7 +9,7 @@ function initDarkMode(): string {
     // document.documentElement.classList.remove('dark')
     localStorage.theme = 'light'
   }
-  return localStorage.theme;
+  // return localStorage.theme;
 
   // Whenever the user explicitly chooses light mode
   // localStorage.theme = 'light'
@@ -22,16 +22,20 @@ function initDarkMode(): string {
 }
 
 function DarkModeToggle({ darkMode, setDarkMode }: { darkMode: boolean, setDarkMode: Function }) {
+  initDarkMode();
+
   function toggleDarkMode() {
     setDarkMode(!darkMode);
     localStorage.theme = localStorage.theme === 'dark' ? 'light' : 'dark';
   }
 
   return (
-    <button className="p-1 rounded-full bg-gray-50 focus-visible:outline-indigo-400 dark:bg-yellow-50 dark:focus-visible:outline-yellow-400">
-      {darkMode
-        ? <SunIcon className="h-6 w-6 text-yellow-400" onClick={toggleDarkMode} />
-        : <MoonIcon className="h-6 w-6 text-indigo-400" onClick={toggleDarkMode} />}
+    <button onClick={toggleDarkMode} className="relative border border-gray-400 p-1 w-12 h-6 rounded-full bg-gray-50 focus-visible:outline-indigo-400 dark:bg-gray-600 dark:focus-visible:outline-yellow-400">
+      <div className="absolute top-1/2 left-0 dark:left-full dark:-translate-x-full -translate-y-1/2 transition-[left,transform] duration-300 ease-out">
+          {darkMode
+            ? <SunIcon className="h-5 w-5 text-yellow-400" />
+            : <MoonIcon className="h-5 w-5 text-indigo-400" />}
+      </div>
     </button>
   )
 }
