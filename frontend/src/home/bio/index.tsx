9528bf566a4053ab59e8ccfcd4a45ef2dfd4667d
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-07 16:29:33 -0800

    Bio section mobile friendly update

--- frontend/src/home/bio/index.tsx
+++ frontend/src/home/bio/index.tsx
@@ -9,7 +9,7 @@ export default function Bio() {
   const [selectedTab, setSelectedTab] = useState<Tab>('experience');
 
   return (
-    <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col items-center p-6 text-gray-600 dark:text-gray-300">
+    <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col items-center p-6 lg:px-12 text-gray-600 dark:text-gray-300">
       <IdentificationIcon className="h-8 w-8 text-yellow-400" />
       <div className="mt-4 lg:max-w-xl">I'm a fullstack software engineer with 3 years of experience, specializing in frontend, but eager to continue learning backend.</div>
       <div className="flex flex-col lg:flex-row mt-6 lg:mt-12">
