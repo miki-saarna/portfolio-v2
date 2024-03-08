25362384fe5f4bb00657e02cc1e7a9a6407b9c87
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-08 03:53:56 -0800

    Created non mobile nav bar

--- frontend/src/header/index.tsx
+++ frontend/src/header/index.tsx
@@ -2,6 +2,7 @@ import { JSX, useState } from 'react';
 import DarkModeToggle from '../utils/darkMode';
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import MobileNavMenu from '../nav-menu/mobile';
+import { NavLink } from 'react-router-dom';
 
 function App({ darkMode, setDarkMode }: { darkMode: boolean, setDarkMode: Function }): JSX.Element {
   const [mobileNavMenuOpen, setMobileNavMenuOpen] = useState<boolean>(false);
@@ -10,6 +11,12 @@ function App({ darkMode, setDarkMode }: { darkMode: boolean, setDarkMode: Functi
     <>
       <div className="max-w-2xl lg:max-w-6xl mx-auto flex justify-between items-center h-16 lg:h-24 p-6 lg:px-12">
         <div className="text-5xl lg:text-6xl font-bold">miki</div>
+        <div className="hidden lg:flex space-x-12 justify-between w-full max-w-md text-xl font-bold text-indigo-400">
+          <NavLink to="/">home</NavLink>
+          <NavLink to="/bio">bio</NavLink>
+          <NavLink to="/projects">projects</NavLink>
+          <NavLink to="/contact">contact</NavLink>
+        </div>
         <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} className="hidden lg:block" />
         <button onClick={() => setMobileNavMenuOpen(true)} className="lg:hidden w-8 h-8">
           <Bars3Icon />
