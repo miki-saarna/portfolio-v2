daec7858197ab5ff832053cfa8c156bf0c797cca
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-08 00:07:53 -0800

    Created footer section

---
+++ frontend/src/footer/index.tsx
@@ -0,0 +1,25 @@
+import { JSX } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Footer(): JSX.Element {
+  return (
+    <div className="max-w-2xl lg:max-w-6xl mx-auto p-6 lg:px-12 mt-24 pb-40">
+      <div className="flex flex-col lg:flex-row justify-between lg:items-center">
+        <div className="lg:flex-[3] text-5xl lg:text-6xl font-bold">
+          Let's build something exciting together.
+        </div>
+        <div className="lg:flex-[1] flex flex-col items-end space-y-2 mt-6 lg:text-lg">
+          <NavLink to="/" className="text-indigo-400 flex items-center">home</NavLink>
+          <NavLink to="/bio" className="text-indigo-400">bio</NavLink>
+          <NavLink to="/projects" className="text-indigo-400">projects</NavLink>
+          <NavLink to="/contact" className="text-indigo-400">contact</NavLink>
+        </div>
+      </div>
+      <div className="flex justify-around mx-auto mt-12 lg:mt-24 max-w-sm">
+        <div className="w-8 h-8 rounded-full bg-gray-200"></div>
+        <div className="w-8 h-8 rounded-full bg-gray-200"></div>
+        <div className="w-8 h-8 rounded-full bg-gray-200"></div>
+      </div>
+    </div>
+  )
+}
