bd83af735352fe820d3b3ce2051b85c38391b511
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-07 19:11:44 -0800

    Moved hero section to separate directory

--- frontend/src/home/index.tsx
+++ frontend/src/home/index.tsx
@@ -1,12 +1,14 @@
 import { JSX } from 'react';
-import HeroBanner from './hero';
+import HeroBanner from './hero/index';
 import Bio from './bio/index';
+import Projects from './projects/index'
 
 function Home(): JSX.Element {
   return (
     <div>
       <HeroBanner />
       <Bio />
+      <Projects />
     </div>
   );
 }
