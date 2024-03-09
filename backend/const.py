dff675b2b234fc11369115e59659aea868e3d6ca
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-08 22:34:51 -0800

    Use of const for .env values

---
+++ backend/const.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+REGION_NAME = os.environ["REGION_NAME"]
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+# GMAIL_API_CLIENT_ID = os.environ["GMAIL_API_CLIENT_ID"]
+# GMAIL_API_CLIENT_SECRET = os.environ["GMAIL_API_CLIENT_SECRET"]
