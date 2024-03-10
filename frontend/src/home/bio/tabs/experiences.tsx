eb3623579dab47c44caee11e6e4d5d47d4042367
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-10 12:24:27 -0700

    Added exp images

--- frontend/src/home/bio/tabs/experiences.tsx
+++ frontend/src/home/bio/tabs/experiences.tsx
@@ -1,12 +1,32 @@
-import { JSX } from 'react';
+import { JSX, useEffect } from 'react';
+import { useImages } from '../../../state/images';
+import { GetImageFromS3 } from '../../../utils/index';
 
 export default function Experiences(): JSX.Element {
+
+  const { imageSrcMap, setImageSrc } = useImages();
+
+  useEffect(() => {
+    if (!imageSrcMap.corvee) {
+      const url = `http://127.0.0.1:5000/get-image-url/${'mikito-saarna-portfolio'}/${'corvee-logo-tiny.png'}`;
+      GetImageFromS3('corvee', url, setImageSrc);
+    }
+    if (!imageSrcMap.simplyByPapo) {
+      const url = `http://127.0.0.1:5000/get-image-url/${'mikito-saarna-portfolio'}/${'papo-logo-tiny.png'}`;
+      GetImageFromS3('simplyByPapo', url, setImageSrc);
+    }
+  }, [])
+
   return (
     <div className="flex flex-col space-y-6 divide-y mt-6 lg:mt-0 lg:ml-12">
       <div className="flex">
-        {/* img */}
-        <div className="h-8 min-w-8 rounded-full bg-gray-200"></div>
-        <div className="flex flex-col ml-6 text-left">
+        <div className="min-w-8 mr-6">   
+          {imageSrcMap.corvee
+            ? <img src={imageSrcMap.corvee} alt="Speed Run Ethereum logo" className="w-8 h-8 rounded-full" />
+            : <div className="h-8 min-w-8 rounded-full bg-gray-200 animate-pulse" />
+          }
+        </div>
+        <div className="flex flex-col text-left">
           <div className="text-xl font-bold">UI Developer</div>
           <div className="mt-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repellat sint aperiam, ipsum nulla molestiae neque cumque non hic voluptates</div>
           <div className="mt-2 text-base font-bold">Corvee</div>
@@ -14,9 +34,13 @@ export default function Experiences(): JSX.Element {
         </div>
       </div>
       <div className="flex pt-6">
-        {/* img */}
-        <div className="h-8 min-w-8 rounded-full bg-gray-200"></div>
-        <div className="flex flex-col ml-6 text-left">
+        <div className="min-w-8 mr-6">
+          {imageSrcMap.corvee
+            ? <img src={imageSrcMap.corvee} alt="Speed Run Ethereum logo" className="w-8 h-8 rounded-full" />
+            : <div className="h-8 min-w-8 rounded-full bg-gray-200 animate-pulse" />
+          }
+        </div>
+        <div className="flex flex-col text-left">
           <div className="text-xl font-bold">Junior Frontend Engineer</div>
           <div className="mt-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repellat sint aperiam, ipsum nulla molestiae neque cumque non hic voluptates</div>
           <div className="mt-2 text-base font-bold">Corvee</div>
@@ -24,9 +48,13 @@ export default function Experiences(): JSX.Element {
         </div>
       </div>
       <div className="flex pt-6">
-        {/* img */}
-        <div className="h-8 min-w-8 rounded-full bg-gray-200"></div>
-        <div className="flex flex-col ml-6 text-left">
+        <div className="min-w-8 mr-6">   
+          {imageSrcMap.simplyByPapo
+            ? <img src={imageSrcMap.simplyByPapo} alt="Speed Run Ethereum logo" className="w-8 h-8 rounded-full" />
+            : <div className="h-8 min-w-8 rounded-full bg-gray-200 animate-pulse" />
+          }
+        </div>
+        <div className="flex flex-col text-left">
           <div className="text-xl font-bold">Backend Engineer</div>
           <div className="mt-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repellat sint aperiam, ipsum nulla molestiae neque cumque non hic voluptates</div>
           <div className="mt-2 text-base font-bold">Simply by Papo</div>
--- frontend/src/state/images.js
+++ frontend/src/state/images.js
@@ -6,14 +6,24 @@ export const useImages = () => useContext(ImageContext);
 
 export const ImageProvider = ({ children }) => {
   const [speedRunEth, setSpeedRunEth] = useState(null);
+  const [corvee, setCorvee] = useState(null);
+  const [simplyByPapo, setSimplyByPapo] = useState(null);
 
   const imageSrcMap = {
-    speedRunEth
+    speedRunEth,
+    corvee,
+    simplyByPapo
   }
 
   function setImageSrc(key, src) {
     if (key === 'speedRunEth') {
       setSpeedRunEth(src)
+    }
+    if (key === 'corvee') {
+      setCorvee(src)
+    }
+    if (key === 'simplyByPapo') {
+      setSimplyByPapo(src)
     }
   };
 
