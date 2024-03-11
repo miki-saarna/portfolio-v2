26b7dfc978523ed459f7d050703eb5a097c949d8
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-11 16:35:42 -0700

    Update hero banner spacing and positioning

--- frontend/src/home/hero/index.tsx
+++ frontend/src/home/hero/index.tsx
@@ -17,25 +17,32 @@ export default function HeroBanner(): JSX.Element {
     <div>
 
       {/* headline */}
-      <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col lg:flex-row lg:justify-between items-start p-6 lg:px-12 mt-12 lg:mt-16 text-gray-600 dark:text-gray-300">
-        <div className="text-left text-4xl font-bold">
+      <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col lg:flex-row lg:justify-between items-start pt-16 px-6 lg:px-12 lg:mt-12 lg:mt-6 text-gray-600 dark:text-gray-300">
+        {/* 1 */}
+        <div className="text-center lg:text-left text-4xl font-bold" style={{ flex: 1 }}>
           <div>
             Hello,
           </div>
           <div>
             I'm Miki
           </div>
+
+          <div className="lg:hidden mt-6 mb-3 text-lg font-medium">
+            I love to expand my knowledge of programming and enjoy exploring through building new projects.
+          </div>
         </div>
-        <div className="w-4/5 lg:w-72 mt-4 lg:mt-0 text-left lg:text-center text-sm lg:text-base">
+
+      {/* 3: img */}
+      {imageSrcMap.portrait
+        ? <img src={imageSrcMap.portrait} alt="Speed Run Ethereum logo" className="mx-auto w-96" style={{ flex: 1 }} />
+        : <div className="flex justify-center items-center mx-auto h-[450px] w-96 bg-gray-200 text-gray-400 rounded-md animate-pulse">Portrait loading</div>
+      }
+        {/* 2 */}
+        <div className="hidden lg:block text-center text-lg" style={{ flex: 1 }}>
           I love to expand my knowledge of programming and enjoy exploring through building new projects.
         </div>
       </div>
 
-      {/* img */}
-      {imageSrcMap.portrait
-        ? <img src={imageSrcMap.portrait} alt="Speed Run Ethereum logo" className="mx-auto w-96" />
-        : <div className="flex justify-center items-center mx-auto h-[450px] w-96 bg-gray-200 text-gray-400 rounded-md animate-pulse">Portrait loading</div>
-      }
     </div>
   );
 }
