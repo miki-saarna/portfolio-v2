6b26a144323048dd06a540b9037b5d07fed3bd08
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-11 16:17:07 -0700

    Updated bio section style

--- frontend/src/home/bio/index.tsx
+++ frontend/src/home/bio/index.tsx
@@ -4,11 +4,11 @@ import Tabs from './tabs/index';
 
 export default function Bio(): JSX.Element {
   return (
-    <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col items-center p-6 lg:px-12 text-gray-600 dark:text-gray-300">
-      <IdentificationIcon className="h-8 w-8 text-yellow-400" />
-      <div className="mt-4 lg:max-w-xl text-center">I'm a fullstack software engineer with 3 years of experience, specializing in frontend, but eager to continue learning backend.</div>
-      <Tabs />
-
+    <div className="bg-white">
+      <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col items-center py-12 px-6 lg:px-12 text-gray-600 dark:text-gray-300">
+        <div className="lg:max-w-xl text-center">I'm a fullstack software engineer with 3 years of experience, specializing in frontend, but eager to continue learning backend.</div>
+        <Tabs />
+      </div>
     </div>
   )
 }
