26b1098e6001a10aeffe1b0827e210d91c4f3f3f
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-12 16:38:41 -0700

    Added border for dark mode bio

--- frontend/src/home/bio/index.tsx
+++ frontend/src/home/bio/index.tsx
@@ -5,9 +5,11 @@ import Tabs from './tabs/index';
 export default function Bio(): JSX.Element {
   return (
     <div className="bg-white dark:bg-[#111827] transition-[background-color] duration-200 ease-in-out">
-      <div className="max-w-2xl lg:max-w-6xl mx-auto flex flex-col items-center py-16 px-6 lg:px-12">
-        <div className="lg:max-w-xl text-center text-text-light dark:text-gray-300">I'm a fullstack software engineer with 3 years of experience, specializing in frontend, but eager to continue learning backend.</div>
-        <Tabs />
+      <div className="max-w-2xl lg:max-w-6xl mx-auto px-6 lg:px-12">
+        <div className="flex flex-col items-center py-16 dark:border-y">
+          <div className="lg:max-w-xl text-center text-text-light dark:text-gray-300">I'm a fullstack software engineer with 3 years of experience, specializing in frontend, but eager to continue learning backend.</div>
+          <Tabs />
+        </div>
       </div>
     </div>
   )
