b5743c09dfed87f05222123a681b359ed1f33eeb
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-13 18:01:01 -0700

    Created home route

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 
 bp = Blueprint("utils", __name__)
 
+@bp.route("/")
+def home():
+  return "home"
+
 @bp.route("/not-found")
 def notFound():
   return { "quote": "You cannot find yourself till you lose yourself." }
